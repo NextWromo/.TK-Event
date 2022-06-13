923d6419f379597629fe423d9264495700bb9cab
Team Wromo <support@wromo.com>
2022-06-13 12:23:57 +0200

    add tk

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,60 +14,60 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://hallo.tk';
-export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'WromoShop';
-export const BRAND_NAME = 'TK';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'TK Event';
-export const META_DESCRIPTION =
-  'This is an open source TK Event developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
-export const SITE_DESCRIPTION = 
-  'Next Event by hallo.tk online experience by the community, free for everyone.';
-export const DATE = '02 August 2022';
-export const SHORT_DATE = 'Aug 2 - 9:00am PST';
-export const FULL_DATE = 'Aug 1st 9am Pacific Time (GMT-7)';
-export const TWEET_TEXT = META_DESCRIPTION;
-export const COOKIE = 'user-id';
-
-// Remove process.env.NEXT_PUBLIC_... below and replace them with
-// strings containing your own privacy policy URL and copyright holder name
-export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
-export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
-
-export const CODE_OF_CONDUCT =
-  'https://wromo.com/exploring-atlantas-modern-homes';
-export const REPO = 'https://github.com/NextWromo/.TK-Event';
-export const SAMPLE_TICKET_NUMBER = 1234;
-export const NAVIGATION = [
-  {
-    name: 'Live Stage',
-    route: '/stage/a'
-  },
-  {
-    name: 'Vercel Stage',
-    route: '/stage/c'
-  },
-  {
-    name: 'Event Stage',
-    route: '/stage/m'
-  },
-  {
-    name: 'Schedule',
-    route: '/schedule'
-  },
-  {
-    name: 'Speakers',
-    route: '/speakers'
-  },
-  {
-    name: 'Expo',
-    route: '/expo'
-  },
-  {
-    name: 'Jobs',
-    route: '/jobs'
-  }
-];
-
-export type TicketGenerationState = 'default' | 'loading';
+ export const SITE_URL = 'https://hallo.tk';
+ export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
+ export const TWITTER_USER_NAME = 'WromoShop';
+ export const BRAND_NAME = 'TK';
+ export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
+ export const SITE_NAME = 'TK Event';
+ export const META_DESCRIPTION =
+   'This is an open source TK Event developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+ export const SITE_DESCRIPTION = 
+   'Next Event by hallo.tk online experience by the community, free for everyone.';
+ export const DATE = '02 August 2022';
+ export const SHORT_DATE = 'Aug 2 - 9:00am PST';
+ export const FULL_DATE = 'Aug 1st 9am Pacific Time (GMT-7)';
+ export const TWEET_TEXT = META_DESCRIPTION;
+ export const COOKIE = 'user-id';
+ 
+ // Remove process.env.NEXT_PUBLIC_... below and replace them with
+ // strings containing your own privacy policy URL and copyright holder name
+ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
+ export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
+ 
+ export const CODE_OF_CONDUCT =
+   'https://wromo.com/exploring-atlantas-modern-homes';
+ export const REPO = 'https://github.com/NextWromo/.TK-Event';
+ export const SAMPLE_TICKET_NUMBER = 1234;
+ export const NAVIGATION = [
+   {
+     name: 'Live Stage',
+     route: '/stage/a'
+   },
+   {
+     name: 'Vercel Stage',
+     route: '/stage/c'
+   },
+   {
+     name: 'Event Stage',
+     route: '/stage/m'
+   },
+   {
+     name: 'Schedule',
+     route: '/schedule'
+   },
+   {
+     name: 'Speakers',
+     route: '/speakers'
+   },
+   {
+     name: 'Expo',
+     route: '/expo'
+   },
+   {
+     name: 'Jobs',
+     route: '/jobs'
+   }
+ ];
+ 
+ export type TicketGenerationState = 'default' | 'loading';
