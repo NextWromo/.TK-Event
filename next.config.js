1c5dc51105372719a353453fede06f4105adec2e
Team Wromo <support@wromo.com>
2022-07-06 17:28:38 +0200

    next.

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7005479426047475"
+     crossorigin="anonymous"></script>
+
 module.exports = {
   images: {
     domains: [
