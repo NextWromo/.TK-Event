4fe474b39e78115e3038498ff6d0ce179d715d46
Team Wromo <support@wromo.com>
2022-07-08 12:06:07 +0200

    head

--- components/header.tsx
+++ components/header.tsx
@@ -21,6 +21,9 @@ type Props = {
   description: React.ReactNode;
 };
 
+  script(async, src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7005479426047475', crossorigin='anonymous');
+
+
 export default function Header({ hero, description }: Props) {
   return (
     <>
