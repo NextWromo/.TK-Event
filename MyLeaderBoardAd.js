a1e629f3adaf3cfd2fe42f5194f08108c061473e
Team Wromo <support@wromo.com>
2022-07-08 15:27:34 +0200

    save

---
+++ MyLeaderBoardAd.js
@@ -0,0 +1,17 @@
+
+class MyLeaderBoardAd extends Component {
+
+    componentDidMount() {
+     (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+
+   render () {
+    return(
+        <div>
+        <ins className = "adsbygoogle"
+                style = { {display:"inline-block",width:"728px",height:"90px"} }
+                data-ad-client = "ca-pub-7005479426047475"
+                data-ad-slot = ""></ins>
+        </div>)
+    }
+}
--- a.js
+++
@@ -1,2 +0,0 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7005479426047475"
-     crossorigin="anonymous"></script>
